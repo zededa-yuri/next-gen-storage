c0e90dfe67f251a6dd791fcf00b8b2cad359956f
Yuri Volchkov <yuri@zededa.com>
2021-06-28 14:18:46 +0000

    virtual-host-init: enable debug prints for vhost
    Signed-off-by: Yuri Volchkov <yuri@zededa.com>

--- virtual-host-init.sh
+++ virtual-host-init.sh
@@ -3,7 +3,8 @@
 function enable_kernel_debug {
     echo 8 8 8 8 > /proc/sys/kernel/printk
     echo "file drivers/nvme/* +p" > /sys/kernel/debug/dynamic_debug/control 
-    echo "file drivers/target/* +p" > sudo tee /sys/kernel/debug/dynamic_debug/control 
+    echo "file drivers/target/* +p" > /sys/kernel/debug/dynamic_debug/control
+    echo "file drivers/vhost/* +p" > /sys/kernel/debug/dynamic_debug/control
 }
 
 enable_kernel_debug
