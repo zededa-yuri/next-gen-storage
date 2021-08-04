4fb66d1359f264b470757c1720c4d6bd46d0006e
Yuri Volchkov <yuri@zededa.com>
2021-08-04 15:25:05 +0000

    bench: add netmiko
    Signed-off-by: Yuri Volchkov <yuri@zededa.com>

--- scripts/autobench/bench.py
+++ scripts/autobench/bench.py
@@ -1,10 +1,23 @@
 import click
+from netmiko import ConnectHandler
+import os
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-def bench():
-    print("hello")
+@click.option('--target', help='Target host to run', required=True)
+def bench(target):
+    key_file = os.path.expanduser("~/.ssh/id_rsa")
+    host = {
+        'device_type': 'generic',
+        'host':   target,
+        'username': 'yuri',
+        "use_keys": True,
+        "key_file": key_file,
+    }
 
+    host_connection = ConnectHandler(**host)
+    output = host_connection.send_command('uname')
+    print(output)
