050e904b4002df3089af92af97b6ac3d3231845b
Yuri Volchkov <yuri@zededa.com>
2021-10-12 18:33:13 +0000

    run qemu with zvol attached via vhost

--- autobench/qemuTarget.go
+++ autobench/qemuTarget.go
@@ -17,6 +17,9 @@ import (
 	"github.com/zededa-yuri/nextgen-storage/autobench/qemutmp"
 	"golang.org/x/crypto/ssh"
 	zfs "github.com/bicomsystems/go-libzfs"
+	"io/ioutil"
+	"strings"
+	"math/rand"
 )
 
 type QemuCommand struct {
@@ -40,6 +43,7 @@ type VmConfig struct {
 	Memory       string // default "512"
 	Kernel       string // default ""
 	Password     string // default "asdfqwer"
+	VhostWWPN    string
 }
 
 type VirtM struct {
@@ -138,13 +142,14 @@ func qemuVmRun(vm VirtM, qemuConfigDir string) {
 	vm.cancel()
 }
 
-func (t *VMlist) AllocateVM(ctx context.Context, totalTime time.Duration) error {
+func (t *VMlist) AllocateVM(ctx context.Context, totalTime time.Duration, VhostWWPN string) error {
 	templateArgs := VmConfig{
 		FileLocation: qemuCmd.CFileLocation,
 		Format:       qemuCmd.CFormat,
 		VCpus:        qemuCmd.CVCpus,
 		Memory:       qemuCmd.CMemory,
 		Password:     qemuCmd.CPassword,
+		VhostWWPN:    VhostWWPN,
 	}
 
 	qemuConfigDir := filepath.Join(getSelfPath(), "qemu.cfg")
@@ -261,6 +266,168 @@ func SetupDiskZfs(ctx context.Context, target string) error {
 	return nil
 }
 
+func waitForFile(fileName string) error {
+	maxDelay := time.Second * 5
+	delay := time.Millisecond * 500
+	var waited time.Duration
+	for {
+		if delay != 0 {
+			time.Sleep(delay)
+			waited += delay
+		}
+		if _, err := os.Stat(fileName); err == nil {
+			return nil
+		} else {
+			if waited > maxDelay {
+				return fmt.Errorf("file not found: error %v", err)
+			}
+			delay = 2 * delay
+			if delay > maxDelay {
+				delay = maxDelay
+			}
+		}
+	}
+}
+
+const (
+	tgtPath    = "/sys/kernel/config/target"
+	iBlockPath = tgtPath + "/core/iblock_0"
+	naaPrefix  = "5001405" // from rtslib-fb
+)
+
+// VHostCreateIBlock - Create vHost fabric
+func VHostCreateIBlock(tgtName, wwn string) error {
+	targetRoot := filepath.Join(iBlockPath, tgtName)
+	if _, err := os.Stat(targetRoot); err != nil {
+		return fmt.Errorf("tgt access error (%s): %s", targetRoot, err)
+	}
+	vhostRoot := filepath.Join(tgtPath, "vhost", wwn, "tpgt_1")
+	vhostLun := filepath.Join(vhostRoot, "lun", "lun_0")
+	err := os.MkdirAll(vhostLun, os.ModeDir)
+	if err != nil {
+		return fmt.Errorf("cannot create vhost: %v", err)
+	}
+	controlCommand := "scsi_host_id=1,scsi_channel_id=0,scsi_target_id=0,scsi_lun_id=0"
+	if err := ioutil.WriteFile(filepath.Join(targetRoot, "control"), []byte(controlCommand), 0660); err != nil {
+		return fmt.Errorf("error set control: %v", err)
+	}
+	if err := waitForFile(filepath.Join(vhostRoot, "nexus")); err != nil {
+		return fmt.Errorf("error waitForFile: %v", err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(vhostRoot, "nexus"), []byte(wwn), 0660); err != nil {
+		return fmt.Errorf("error set nexus: %v", err)
+	}
+	if _, err := os.Stat(filepath.Join(vhostLun, "iblock")); os.IsNotExist(err) {
+		if err := os.Symlink(targetRoot, filepath.Join(vhostLun, "iblock")); err != nil {
+			return fmt.Errorf("error create symlink: %v", err)
+		}
+	}
+	return nil
+}
+
+func VHostDeleteIBlock(wwn string) error {
+	vhostRoot := filepath.Join(tgtPath, "vhost", wwn, "tpgt_1")
+	vhostLun := filepath.Join(vhostRoot, "lun", "lun_0")
+	if _, err := os.Stat(vhostLun); os.IsNotExist(err) {
+		return fmt.Errorf("vHost do not exists for wwn %s: %s", wwn, err)
+	}
+	if err := os.Remove(filepath.Join(vhostLun, "iblock")); err != nil {
+		return fmt.Errorf("error delete symlink: %v", err)
+	}
+	if err := os.RemoveAll(vhostLun); err != nil {
+		return fmt.Errorf("error delete lun: %v", err)
+	}
+	if err := os.RemoveAll(vhostRoot); err != nil {
+		return fmt.Errorf("error delete lun: %v", err)
+	}
+	if err := os.RemoveAll(filepath.Dir(vhostRoot)); err != nil {
+		return fmt.Errorf("error delete lun: %v", err)
+	}
+	return nil
+}
+
+func TargetCreateIBlock(dev, tgtName, serial string) error {
+	targetRoot := filepath.Join(iBlockPath, tgtName)
+	err := os.MkdirAll(targetRoot, os.ModeDir)
+	if err != nil {
+		return fmt.Errorf("cannot create fileio: %v", err)
+	}
+	if err := waitForFile(filepath.Join(targetRoot, "control")); err != nil {
+		return fmt.Errorf("error waitForFile: %v", err)
+	}
+	controlCommand := fmt.Sprintf("udev_path=%s", dev)
+	if err := ioutil.WriteFile(filepath.Join(targetRoot, "control"), []byte(controlCommand), 0660); err != nil {
+		return fmt.Errorf("error set control: %v", err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(targetRoot, "wwn", "vpd_unit_serial"), []byte(serial), 0660); err != nil {
+		return fmt.Errorf("error set vpd_unit_serial: %v", err)
+	}
+	if err := ioutil.WriteFile(filepath.Join(targetRoot, "enable"), []byte("1"), 0660); err != nil {
+		return fmt.Errorf("error set enable: %v", err)
+	}
+	return nil
+}
+
+func GetSerialTarget(tgtName string) (string, error) {
+	targetRoot := filepath.Join(iBlockPath, tgtName)
+	//it returns something like "T10 VPD Unit Serial Number: 5001405043a8fbf4"
+	serial, err := ioutil.ReadFile(filepath.Join(targetRoot, "wwn", "vpd_unit_serial"))
+	if err != nil {
+		return "", fmt.Errorf("GetSerialTarget for %s: %s", targetRoot, err)
+	}
+	parts := strings.Fields(strings.TrimSpace(string(serial)))
+	if len(parts) == 0 {
+		return "", fmt.Errorf("GetSerialTarget for %s: empty line", targetRoot)
+	}
+	return parts[len(parts)-1], nil
+}
+
+func IsVhostIblockExist(tgtName string) (bool, error) {
+	serial, err := GetSerialTarget(tgtName)
+	if err != nil {
+		return false, fmt.Errorf("CheckVHostIBlock (%s): %v", tgtName, err)
+	}
+
+	vhostRoot := filepath.Join(tgtPath, "vhost", fmt.Sprintf("naa.%s", serial), "tpgt_1")
+	vhostLun := filepath.Join(vhostRoot, "lun", "lun_0")
+	if _, err := os.Stat(filepath.Join(vhostLun, "iblock")); err == nil {
+		return true, nil
+	}
+	return false, nil
+}
+
+func GenerateNaaSerial() string {
+	return fmt.Sprintf("%s%09x", naaPrefix, rand.Uint32())
+}
+
+func SetupVhost() (string, error) {
+	device := "/dev/zvol/tank/test-zvol"
+	iblock_id := "test_iblock"
+	serial := GenerateNaaSerial()
+	wwn := fmt.Sprintf("naa.%s", serial)
+	err := TargetCreateIBlock(device, iblock_id, serial)
+	if err != nil {
+		return "", fmt.Errorf("TargetCreateFileIODev(%s, %s, %s): %v",
+			device, iblock_id, serial, err)
+	}
+	exists,err := IsVhostIblockExist(iblock_id) 
+	if !exists {
+		err = VHostCreateIBlock(iblock_id, wwn)
+		if err != nil {
+			errString := fmt.Sprintf("VHostCreateIBlock: %v", err)
+			err = VHostDeleteIBlock(wwn)
+			if err != nil {
+				errString = fmt.Sprintf("%s; VHostDeleteIBlock: %v",
+					errString, err)
+			}
+			return "", fmt.Errorf("VHostCreateIBlock(%s, %s): %s",
+				iblock_id, wwn, errString)
+		}
+	}
+	return wwn, nil
+
+}
+
 func RunCommand(ctx context.Context, virtM VMlist) error {
 	err := InitFioOptions()
 	if err != nil {
@@ -272,13 +439,19 @@ func RunCommand(ctx context.Context, virtM VMlist) error {
 	var totalTime = time.Duration(int64(countTests)*int64(60*time.Second) + int64(bufferTime))
 	ctxVMs, cancelVMS := context.WithTimeout(ctx, totalTime)
 
-	err = SetupDiskZfs(ctxVMs, opts.TargetDisk)
+	// err = SetupDiskZfs(ctxVMs, opts.TargetDisk)
+	// if err != nil {
+	// 	cancelVMS()
+	// 	return fmt.Errorf("Can't setup disk %s:%v", opts.TargetDisk, err)
+	// }
+
+	wwpn,err := SetupVhost()
 	if err != nil {
 		cancelVMS()
 		return fmt.Errorf("Can't setup disk %s:%v", opts.TargetDisk, err)
 	}
 
-	err = virtM.AllocateVM(ctxVMs, totalTime)
+	err = virtM.AllocateVM(ctxVMs, totalTime, wwpn)
 	if err != nil {
 		cancelVMS()
 		return fmt.Errorf("VM create in QEMU failed err:%v", err)
