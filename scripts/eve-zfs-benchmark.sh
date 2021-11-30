ed8e471a2a801ba80f3593994cef86aee1d8dc56
Yuri Volchkov <yuri@zededa.com>
2021-11-30 12:03:02 +0000

    update

--- scripts/eve-zfs-benchmark.sh
+++ scripts/eve-zfs-benchmark.sh
@@ -23,9 +23,52 @@ END {
     echo "}"
 }
 
+zfs_get_one_param_json() {
+    param="$1"
+
+    printf "\"%s\": %d" "${param}" "$(cat /sys/module/zfs/parameters/${param})"
+}
+get_zfs_params_json() {
+    echo "{"
+    params="zfs_compressed_arc_enabled \
+zfs_vdev_min_auto_ashift \
+zvol_request_sync \
+zfs_arc_min \
+zfs_arc_max \
+zfs_vdev_aggregation_limit_non_rotating \
+zfs_vdev_async_write_active_min_dirty_percent \
+zfs_vdev_async_write_active_max_dirty_percent \
+zfs_delay_min_dirty_percent \
+zfs_delay_scale \
+zfs_dirty_data_max \
+zfs_dirty_data_sync_percent \
+zfs_prefetch_disable \
+zfs_vdev_sync_read_min_active \
+zfs_vdev_sync_read_max_active \
+zfs_vdev_sync_write_min_active \
+zfs_vdev_sync_write_max_active \
+zfs_vdev_async_read_min_active \
+zfs_vdev_async_read_max_active \
+zfs_vdev_async_write_min_active \
+zfs_vdev_async_write_max_active \
+"
+
+    new_params="zfs_smoothing_scale zfs_write_smoothing"
+    if [ -f /sys/module/zfs/parameters/zfs_smoothing_scale ]; then
+	params="${params}" "${new_params}"
+    fi
+
+    printf "\"version\": \"%s\"" "$(cat /sys/module/zfs/version)"
+    for opt in ${params}; do
+	echo ","
+	zfs_get_one_param_json "${opt}"
+    done
+    echo -e "\n}"
+}
+
 write_dm_tx_json_head() {
     out_file="$1"
-    echo -e "{ \"before_test\":" > "${out_file}"
+    echo -e "{\"before_test\":" > "${out_file}"
     get_dm_tx_json_body >> "${out_file}"
 }
 
@@ -38,7 +81,10 @@ write_dm_tx_json_tail() {
 
 write_sys_stat_json_head() {
     out_file="$1"
-    echo -e "{ \"before_test\":" > "${out_file}"
+    echo -e "{ \"zfs\":" > "${out_file}"
+    get_zfs_params_json >> "${out_file}"
+
+    echo ",\"before_test\":" >> "${out_file}"
     get_sample >> "${out_file}"
 }
 
@@ -96,8 +142,14 @@ monitor() {
     echo "Starting monitor.."
     #trap "echo ] >> ${output}" SIGINT
     trap "echo terminating monitor.. && echo ] >> ${output} && return 0" TERM
+    trap exit 1 KILL
 
     rm -f "${output}"
+    if ! touch "${output}"; then
+	echo "${output} is not accessible"
+	exit 1
+    fi
+
     echo "[" >> "${output}"
     get_sample  >> "${output}"
     while true; do
@@ -108,6 +160,10 @@ monitor() {
 }
 
 setup_ssh_config() {
+    if [ -f /root/.ssh/id_rsa.pub ]; then
+	return
+    fi
+
     cat > /root/.ssh/config <<EOF
 Host guest
   HostName 10.1.0.128
@@ -117,6 +173,8 @@ Host guest
   ControlPersist 10m
   SendEnv FIO_*
 EOF
+
+    cp ssh-keys/* ~/.ssh/
 }
 
 
@@ -132,12 +190,14 @@ one_test_finish() {
 }
 
 one_test() {
-    FIO_rw="${1}"
-    FIO_bs="${2}"
-    FIO_nr_jobs="${3}"
-    FIO_iodepth="${4}"
+    results_dir="${1}"
+    FIO_rw="${2}"
+    FIO_bs="${3}"
+    FIO_nr_jobs="${4}"
+    FIO_iodepth="${5}"
 
-    out_dir="${FIO_rw}-${FIO_nr_jobs}jobs"
+    test_name="${FIO_rw}-jobs${FIO_nr_jobs}-bs${FIO_bs}-iodepth${FIO_iodepth}"
+    out_dir="${results_dir}/${test_name}"
     FIO_OUT_PATH="fio-output/${out_dir}-guest"
 
     zfs_trim || exit 1
@@ -155,7 +215,7 @@ one_test() {
 
     trap "one_test_finish ${out_dir} ${monitor_pid}"  EXIT
 
-    echo "Starting job"
+    echo "Starting job ${out_dir}"
     export FIO_OUT_PATH
     export FIO_bs
     export FIO_rw
@@ -163,10 +223,16 @@ one_test() {
     export FIO_iodepth
 
     sleep 1
-    ssh guest "rm -rf ${FIO_OUT_PATH} && mkdir ${FIO_OUT_PATH}" || exit 1
+    ssh guest "rm -rf ${FIO_OUT_PATH} && mkdir -p ${FIO_OUT_PATH}" || exit 1
 
-    ssh guest "fio fio-template.job --output-format=json+,normal --output=${FIO_OUT_PATH}/result.json"
-    scp -r guest "${FIO_OUT_PATH}" "${out_dir}"
+    ssh guest "fio fio-template.job \
+--output-format=json+,normal \
+--output=${FIO_OUT_PATH}/result.json \
+--group_reporting --eta-newline=1 \
+> ${FIO_OUT_PATH}/fio-log.txt
+"
+    scp -r guest:"${FIO_OUT_PATH}" "${out_dir}"
+    ssh guest "rm -rf ${out_dir}"
     echo "Done"
 }
 
@@ -203,15 +269,44 @@ format_disk() {
 }
 
 main() {
+    results_dir="${1}"
     target_device=sdb
+
+    if ! command -v zpool; then
+	echo "Please run"
+	echo "    apk update && apk add zfs"
+	exit 1
+    fi
+
+    if [ -d "${results_dir}" ]; then
+	read -p "${results_dir} exists, remove? Y/n" yn
+	case $yn in
+            [Yy]* ) echo yes; break;;
+            [Nn]* ) echo no; exit 1;;
+            * ) echo "Please answer yes or no."; exit ;;
+	esac
+    fi
 
     format_disk || exit 1
 
-    one_test write 256 1 1
+    mkdir -p "${results_dir}"
+    # results_dir rw bs jobs_nr iodepth
+    one_test "${results_dir}" write 256k 1 1
 
+    for load in write read randwrite randwrite trimwrite; do
+	for nr_jobs in 2 4; do
+	    for io_depth in 1 4 16; do
+		one_test "${results_dir}" "${load}" 64k "${nr_jobs}" "${io_depth}"
+	    done
+	done
+    done
 }
 
-main
+#get_zfs_params_json
+setup_ssh_config
+main zfs_untuned
 # setup_ssh_config
 # write_sys_stat_json_head test_sys.json
 # write_sys_stat_json_tail test_sys.json
+# write_dm_tx_json_head test_dm_tx.json
+# write_dm_tx_json_tail test_dm_tx.json
